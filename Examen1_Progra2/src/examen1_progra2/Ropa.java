d0846e01615aa2454381fb3569e2953c29cec66c
AngelRisso1337z <angel.risso@unitec.edu>
2017-11-10 13:53:21 -0600

    creando clases herederas de objeto

---
+++ Examen1_Progra2/src/examen1_progra2/Ropa.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1_progra2;
+
+/**
+ *
+ * @author HP_AMD_64
+ */
+public class Ropa {
+    private int talla;
+    private String material;
+}
--- Examen1_Progra2/src/examen1_progra2/Zapatos.java
+++ Examen1_Progra2/src/examen1_progra2/Zapatos.java
@@ -5,10 +5,55 @@
  */
 package examen1_progra2;
 
+import java.awt.Color;
+
 /**
  *
  * @author HP_AMD_64
  */
-public class Zapatos {
+public class Zapatos extends Objeto{
+    private int talla;
+    private String suela;
+    private int confort;
+
+    public Zapatos() {
+        super();
+    }
+
+    public Zapatos(int talla, String suela, int confort, Color color, String descripcion, String marca, int tamagno, int calidad, int precio, String duegno) {
+        super(color, descripcion, marca, tamagno, calidad, precio, duegno);
+        this.talla = talla;
+        this.suela = suela;
+        this.confort = confort;
+    }
+
+    public int getTalla() {
+        return talla;
+    }
+
+    public void setTalla(int talla) {
+        this.talla = talla;
+    }
+
+    public String getSuela() {
+        return suela;
+    }
+
+    public void setSuela(String suela) {
+        this.suela = suela;
+    }
+
+    public int getConfort() {
+        return confort;
+    }
+
+    public void setConfort(int confort) {
+        this.confort = confort;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "talla=" + talla + ", suela=" + suela + ", confort=" + confort + '}';
+    }
     
 }
